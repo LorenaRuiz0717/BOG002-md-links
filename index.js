5a7c6d039bf1f288dad637a2828987ad746636b2
unknown <lorena0717@hotmail.com>
2021-06-21 11:15:50 -0500

    npm inicializado, buscando archivos md y pruebas md

--- index.js
+++ index.js
@@ -1,3 +1,51 @@
-module.exports = () => {
-  // ...
-};
+// module.exports = () => {
+//   // ...
+// };
+
+const fs = require('fs');
+const path = require('path');
+
+function buscandoMd() {
+files = fs.readdirSync(__dirname);
+console.log("\Filenames with the .md extension:");
+files.forEach(file => {
+if (path.extname(file) == ".md")
+	console.log(file);
+})
+
+}
+
+fs.readFile('README.md', 'utf8' , (err, data) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  // console.log(data)
+})
+
+
+
+// const fs = require('fs');
+// const path = require('path');
+
+// function buscandoMd() {
+// files = fs.readdirSync(__dirname);
+// const archivoMd= []
+// console.log("\Filenames with the .md extension:");
+// files.forEach(file => {
+// if (path.extname(file) == ".md")
+// 	console.log('archivoMd '+file);
+//   archivoMd.push(file)
+//   console.log('array'+archivoMd)
+// })
+
+// }
+
+// // fs.readFile('README.md', 'utf8' , (err, data) => {
+// //   if (err) {
+// //     console.error(err)
+// //     return
+// //   }
+// //   console.log(data)
+// // })
+buscandoMd();
