2c0c674ddfbcf121648a07e7a4d4228a302cbb78
unknown <lorena0717@hotmail.com>
2021-06-26 19:55:14 -0500

    siguiendo rutas y buscando archivos md

--- index.js
+++ index.js
@@ -2,50 +2,87 @@
 //   // ...
 // };
 
+// Metodos Propios de Node
 const fs = require('fs');
 const path = require('path');
+// Librerias instaladas
+const marked= require ('marked');
 
-function buscandoMd() {
-files = fs.readdirSync(__dirname);
-console.log("\Filenames with the .md extension:");
-files.forEach(file => {
-if (path.extname(file) == ".md")
-	console.log(file);
-})
+
+//Leer y resolver ruta
+function leerRuta(ruta){
+   if (path.isAbsolute(ruta)===false){// si la ruta no es absoluta utilizar el metodo resolve
+      const rutaAbsoluta =path.resolve(ruta)//resolve determina la ruta absoluta
+      buscandoMd(rutaAbsoluta)//con la ruta resuelta ejecutamos buscandoMd
+      console.log('La ruta ingresada es '+ rutaAbsoluta);
+   }else{
+     buscandoMd(ruta) // si la ruta es absoluta, ejecutamos buscandoMd
+      console.log('Ruta absoluta verficada' +ruta)  
+   }
+  }
+  
+
+// Leer los archivos de la  ruta
+function buscandoMd(rutaIngresada) {//aqui hacer promesa, ¿resolviendo archivosMd?
+const archivosMd=[];
+if(fs.lstatSync(rutaIngresada).isFile()){// si la ruta ingresada es un archivo
+  if(path.extname(rutaIngresada) == ".md"){// se compara su extension con "md"
+    archivosMd.push( //se almacena nombre del archivo y su ruta 
+      {archivo:fs.readFileSync(rutaIngresada),
+        ruta: (rutaIngresada)});
+        console.log('Archivo md encontrado en la ruta '+rutaIngresada)
+        console.log('Archivos md '+archivosMd)
+        leermd(archivosMd)
+  }else{
+    console.log('Archivo md no encontrado en la ruta '+ rutaIngresada)
+    console.log('Archivos md '+Object.values(archivosMd))
+    
+  }
+}else{
+ let  archivos= fs.readdirSync(rutaIngresada);//si la ruta no es un archivo, leemos el directorio
+ archivos.map(archivo=>{
+   const rutaDirectorio=path.join(rutaIngresada,archivo);
+   const todosArchivos= buscandoMd(rutaDirectorio);
+   console.log('Archivos md '+Object.values(archivosMd))
+   leermd(archivosMd)
+  //  archivosMd=archivosMd.concat(todosArchivos);
+ });
+};
+
+// console.log('Archivos md '+Object.values(archivosMd))
+// console.log('Archivos md '+archivosMd.length)
+return archivosMd;
 
 }
 
-fs.readFile('README.md', 'utf8' , (err, data) => {
-  if (err) {
-    console.error(err)
-    return
-  }
-  // console.log(data)
-})
-
-
-
-// const fs = require('fs');
-// const path = require('path');
-
-// function buscandoMd() {
-// files = fs.readdirSync(__dirname);
-// const archivoMd= []
-// console.log("\Filenames with the .md extension:");
+//     
 // files.forEach(file => {
-// if (path.extname(file) == ".md")
-// 	console.log('archivoMd '+file);
-//   archivoMd.push(file)
-//   console.log('array'+archivoMd)
-// })
+// if (path.extname(file) == ".md"){
+// archivosMd.push(file)
 
-// }
+// // console.log(rutaMd)
+// console.log(archivosMd)
+  
+//   // leermd()
+//   })
 
-// // fs.readFile('README.md', 'utf8' , (err, data) => {
-// //   if (err) {
-// //     console.error(err)
-// //     return
-// //   }
-// //   console.log(data)
-// // })
-buscandoMd();
+//leer md
+function leermd(){
+  // console.log('traje'+archivosMd)
+  fs.readFile('README.md', 'utf8' , (err, data) => {
+      if (err) {
+        console.error(err)
+        return
+      }
+      console.log(data)
+    })
+    }
+
+
+
+    leerRuta('archivosMd')
+    leerRuta('README.md')
+
+
+
+
